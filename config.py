49d7b61570f3768136f7324657fee63ed4b8396d
rotichkipchirchir65-pixel <rotichkipchirchir65@gmail.com>
2025-09-02 21:16:17 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py
+
+DERIV_WS_URL = "wss://ws.derivws.com/websockets/v3?app_id=1089"
+
+SYMBOLS = {
+    "Volatility 10 (1s)": "R_10",
+    "Volatility 25 (1s)": "R_25",
+    "Volatility 50 (1s)": "R_50",
+    "Volatility 75 (1s)": "R_75",
+    "Volatility 100 (1s)": "R_100"
+}
+
+FAST_MA = 5
+SLOW_MA = 20
